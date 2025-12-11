1664e2bd286c88bb0c2e23450766266baacb0888
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-12-10 18:54:54 -0700

    Create category route with CRUD

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,126 @@
+import express from "express";
+const router = express.Router();
+import Category from "../models/Category.js";
+import mongoose from "mongoose";
+
+// GET /api/categories
+// Returns a list of categories
+// Example response: [{ id: 1, name: "Electronics" }, { id: 2, name: "Clothing" }]
+// access: public
+router.get("/", async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400);
+      throw new Error("Invalid category ID");
+    }
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      res.status(404);
+      throw new Error("Category not found");
+    }
+    res.status(200).json({
+      succsess: true,
+      data: category,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /api/categories
+/// Creates a new category
+// Example request body: { name: "Toys" }
+// Example response: { message: "Category created successfully" }
+// access: private
+router.post("/", async (req, res) => {
+  const { name, description } = req.body;
+  if (!name?.trim() || !description.trim()) {
+    res.status(400);
+    throw new Error("Category name descriont are required to fill");
+  }
+
+  const newCategory = new Category({
+    name: name.trim(),
+    description: description.trim(),
+  });
+
+  const savedCategory = await newCategory.save();
+  console.log("Category created", req.body);
+  res.status(201).json({ succsess: true, data: savedCategory });
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400);
+      throw new Error("Invalid category ID");
+    }
+
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) {
+      res.status(404);
+      throw new Error("Category not found");
+    }
+
+    res.status(200).json({
+      succsess: true,
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400);
+      throw new Error("Invalid category ID");
+    }
+
+    const category = await Category.findById(id);
+    if (!category) {
+      res.status(404);
+      throw new Error("Category not found");
+    }
+
+    category.name = name?.trim() || category.name;
+    category.description = description?.trim() || category.description;
+
+    // console.log("Updating category:", category);
+
+    const updatedCategory = await Category.findByIdAndUpdate(id, category, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedCategory) {
+      res.status(404);
+      throw new Error("Category not found after update");
+    }
+
+    res.status(200).json({
+      succsess: true,
+      data: updatedCategory,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import { errorHandler } from "./middleware/errorHandler.js";
+
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 
 const app = express();
+// Connect to database
+connectDB();
 const PORT = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Define a simple route
-app.get("/", (req, res) => {
-  res.send("Welcome to the Shopping Backend API");
+// Import category routes
+
+app.use("/api/categories", categoryRoutes);
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404);
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  next(error);
 });
+
+// Error handling middleware
+app.use(errorHandler);
 
 // Start the server
 app.listen(PORT, () => {
